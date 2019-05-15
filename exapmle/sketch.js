65753383477e0aa87393899bde5f5dc8037bdea2
soriYang <sorizzang_@naver.com>
2019-05-15 11:59:21 +0900

    example_file

---
+++ exapmle/sketch.js
@@ -0,0 +1,24 @@
+function setup() {
+  title = createElement('h2', "<a href='/PoeticCodeForNature'> HOME : </a> 작품 제목");
+  title.position(20, 0);
+
+  canvas = createCanvas(500, 500);
+  canvas.position(20, 60);
+  canvas.class("artwork");
+
+  description = "\
+  작품에 대한 설명이 들어갑니다. <br/> \
+  HTML이 직접 들어가서 줄넘김을 할 수 있습니다. \
+  ";
+  text = createDiv(description);
+  text.position(20, 400);
+  text.style("font-family", "monospace");
+  text.style("font-size", "12pt");
+
+}
+
+function draw() {
+  background(0);
+  fill(255);
+  ellipse(width/2, height/2, 50);
+}
